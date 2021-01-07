b4867d970b2db1d3e443a3415b0554ee6e68867a
Chase Hochstrasser <chochstrasser@sofi.org>
2021-01-06 21:53:35 -0700

    stripe and firebase integration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,25 +3,64 @@ import * as admin from 'firebase-admin';
 import { Stripe } from 'stripe';
 
 admin.initializeApp();
+const config = functions.config();
 
-const stripe = new Stripe(functions.config().stripe.secret, {
+const stripe = new Stripe(config.stripe.secret, {
   apiVersion: '2020-08-27',
 });
 
 export const createCheckoutSession = functions.https.onCall(
-  async (data, context) => {
+  async (data: any, context: any) => {
     if (!data) return;
     const session = await stripe.checkout.sessions.create({
+      customer: data.customer,
+      customer_email: data.customer_email,
       payment_method_types: ['card'],
-      line_items: data,
+      line_items: data.line_items,
       mode: 'payment',
       success_url: 'http://localhost:4200/success',
       cancel_url: 'http://localhost:4200/cancel',
+      metadata: {
+        uid: data.uid,
+      },
     });
     return session.id;
   }
 );
 
-export const events = functions.https.onRequest((request, response) => {
-  response.send('Endpoint for Stripe Webhooks!');
-});
+const fulfillOrder = async (session: any) => {
+  await admin
+    .firestore()
+    .collection('customers')
+    .doc(session.metadata.uid)
+    .update({
+      payments: admin.firestore.FieldValue.arrayUnion(session),
+    });
+};
+
+export const events = functions.https.onRequest(
+  async (request: functions.https.Request, response: any) => {
+    const payload = request.rawBody.toString('utf8');
+    const stripeSignature: string = request.headers[
+      'stripe-signature'
+    ] as string;
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        payload,
+        stripeSignature,
+        config.stripe.webhooks_signing
+      );
+    } catch (err) {
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+      await fulfillOrder(session);
+    }
+
+    response.status(200).send('Finished!');
+  }
+);
--- src/app/account-bag/account-bag.component.ts
+++ src/app/account-bag/account-bag.component.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { loadStripe, Stripe } from '@stripe/stripe-js';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { CartService } from '../services/cart.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-account-bag',
@@ -14,26 +15,35 @@ export class AccountBagComponent implements OnInit {
   products: Array<any> = [];
   confirmation: any;
   loading = false;
+  email = '';
 
   constructor(
     private fns: AngularFireFunctions,
-    private cartService: CartService
+    private cartService: CartService,
+    private authService: AuthService
   ) {}
 
   async ngOnInit() {
     this.stripe = await loadStripe(environment.stripeKey);
+    this.email = this.authService.getLocalUserData.email;
   }
 
   async checkout(e: Event) {
     e.preventDefault();
     this.loading = true;
     this.products = this.cartService.getProducts();
-    const data = this.products.map((p) => {
-      return {
-        price: p.alt[p.size][p.color].stripe_price_id,
-        quantity: p.quantity,
-      };
-    });
+    const userInfo = this.authService.getLocalUserData;
+    const data = {
+      uid: userInfo.uid,
+      customer: userInfo.payments[0].customer,
+      customer_email: userInfo.email,
+      line_items: this.products.map((p) => {
+        return {
+          price: p.alt[p.size][p.color].string_price_id,
+          quantity: p.quantity,
+        };
+      }),
+    };
     const createCheckoutSession = this.fns.httpsCallable(
       'createCheckoutSession'
     );
